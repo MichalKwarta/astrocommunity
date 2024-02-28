f4a4a8d80437b9c5c896fec4ffb91a552b60bfdd
Micah Halter <micah@mehalter.com>
2024-02-28 08:45:51 -0500

    perf(vim-matchup): defer matchparen highlighting

--- lua/astrocommunity/motion/vim-matchup/init.lua
+++ lua/astrocommunity/motion/vim-matchup/init.lua
@@ -8,6 +8,7 @@ return {
         opts = {
           options = {
             g = {
+              matchup_matchparen_deferred = 1,
               matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 },
             },
           },
